25fbd36dd07c31db11c0f3a2513f450ecc4aa2da
Anders <72553311+Anders-1@users.noreply.github.com>
2022-02-16 16:28:06 -0500

    init

---
+++ script.js
@@ -0,0 +1,57 @@
+function alertFunction() {
+
+	alert("You clicked me!");
+	console.log("Alerted");
+}
+
+function darkMode() {
+
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+	var button = document.getElementById("darkButton");
+	if (button.innerHTML === "Darkify") {
+		button.innerHTML = "Lightify";
+	} else {
+		button.innerHTML = "Darkify";
+
+	}
+
+}
+
+function magicAnswer() {
+
+	var mad = Math.floor(Math.random() * 10);
+	var guess = Math.floor(Math.random() * 3);
+	var answerText = document.getElementById("answerText")
+	var answer = null
+
+	answerText.style.display = "none"
+
+	if (mad === 0) {
+
+		answer = "Get out of my swamp!"
+		guess = "null"
+	}
+
+	switch (guess) {
+		case 0:
+			answer = "No"
+			break;
+		case 1:
+			answer = "Maybe"
+			break;
+		case 2:
+			answer = "Yes"
+			break;
+		default:
+
+			if (guess != "null") {
+				console.warn("Uh oh, 'guess' is not in range.")
+			}
+
+	}
+
+	answerText.innerHTML = answer
+	answerText.style.display = "block"
+
+}
