b13248a1f4ef10ae404ff490e700098c71678b86
Anders <ahpyenson@gmail.com>
2022-02-25 15:34:26 -0500

    Added semicolons.

--- script.js
+++ script.js
@@ -47,37 +47,37 @@ function magicAnswer() {
 
 	var mad = Math.floor(Math.random() * 10);
 	var guess = Math.floor(Math.random() * 3);
-	var answerText = document.getElementById("answerText")
-	var answer = null
+	var answerText = document.getElementById("answerText");
+	var answer = null;
 
-	answerText.style.display = "none"
+	answerText.style.display = "none";
 
 	if (mad === 0) {
 
-		answer = "Get out of my swamp!"
-		guess = "null"
+		answer = "Get out of my swamp!";
+		guess = "null";
 	}
 
 	switch (guess) {
 		case 0:
-			answer = "No"
+			answer = "No";
 			break;
 		case 1:
-			answer = "Maybe"
+			answer = "Maybe";
 			break;
 		case 2:
-			answer = "Yes"
+			answer = "Yes";
 			break;
 		default:
 
 			if (guess != "null") {
-				console.warn("Uh oh, 'guess' is not in range.")
+				console.warn("Uh oh, 'guess' is not in range.");
 			}
 
 	}
 
-	answerText.innerHTML = answer
-	answerText.style.display = "block"
+	answerText.innerHTML = answer;
+	answerText.style.display = "block";
 
 }
 
